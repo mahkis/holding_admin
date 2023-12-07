8704c3624d6e7cffb9ee3ba9f98aa5879f49a0e5
islombek <mahkis@yandex.ru>
2023-12-07 08:12:45 +0500

    Test

--- resources/views/Admin/all_certificates.blade.php
+++ resources/views/Admin/all_certificates.blade.php
@@ -49,8 +49,7 @@
                             </td>
 
                             <td width="100">
-                                <img
-                                    src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->size(100)->generate('https://admin.holding.uz/check-certificate/'.$row->uuid)) !!} ">
+{{--                                <img src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->size(100)->generate('https://admin.holding.uz/check-certificate/'.$row->uuid)) !!} ">--}}
                             </td>
                             <td>
                                 @if($row->file_id)
